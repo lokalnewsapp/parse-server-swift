9b83d5ca78930ba0e2695f2b7704f4482ae59e3d
Jayson Ng <jayson@jay.ph>
2024-01-13 16:45:33 +0800

    clean json files

--- Sources/ParseServerSwift/RoutesCollections/UserCollection.swift
+++ Sources/ParseServerSwift/RoutesCollections/UserCollection.swift
@@ -9,19 +9,48 @@ import Vapor
 import ParseSwift
 
 struct UserCollection: RouteCollection {
+    
     func boot(routes: RoutesBuilder) throws {
-        routes.group("users") { group in
-            group.get(":username", "hello", use: getWelcome)
-            //group.get(":username", "score", use: getScore)
-        }
+        
+        let usersGroup = routes.grouped("user")
+        usersGroup.post("login", "after",
+                        object: User.self,
+                        trigger: .afterLogin,
+                        use: afterLogin
+        )
+                
+    }
+}
+
+extension Data {
+    var prettyString: NSString? {
+        return NSString(data: self, encoding: String.Encoding.utf8.rawValue) ?? nil
     }
 }
 
 extension UserCollection {
-    func getWelcome(request: Request) -> String {
-        return "welcome"
+    
+    
+    // Another Parse Hook Trigger route.
+    func afterLogin(req: Request) async throws -> ParseHookResponse<Bool> {
+
+        // Note that `ParseHookResponse<Bool>` means a "successful"
+        // response will return a "Bool" type. Bool is the standard response with
+        // a "true" response meaning everything is okay or continue.
+        if let error: ParseHookResponse<Bool> = checkHeaders(req) {
+            return error
+        }
+        let parseRequest = try req.content
+            .decode(ParseHookTriggerObjectRequest<User, User>.self)
+        
+        let user = parseRequest.user
+        let options = try parseRequest.options(req)
+        
+        let userObject = try await User.query("objectId" == user?.objectId).includeAll().first(options: options)
+        
+        req.logger.info("A user has logged in: \(userObject)")
+//        req.logger.info("A user has logged in: \(userObject.logable)")
+        return ParseHookResponse(success: true)
     }
-    func getScore(request: Request) -> String {
-        return "score"
-    }
+    
 }
